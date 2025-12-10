0684c9d38cd802ff93c23be16fe6751d3f24a906
Shrutika Gursale <gursaleshrutika@gmail.com>
2025-12-10 17:17:37 +0530

    hello

---
+++ main.py
@@ -0,0 +1,10 @@
+# import arithmetic
+# from arithmetic import substract
+
+print("Hello world !")
+
+# x = int(input("Enter no 1 : "))
+# y = int(input("Enter no 2 : "))
+
+# arithmetic.add(x,y)
+# substract(x,y)
