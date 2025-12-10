5985011dd1a6a2be3bc74f5144e18fe56c60cb98
Shrutika Gursale <gursaleshrutika@gmail.com>
2025-12-10 17:26:37 +0530

    area perimiter

---
+++ geometry.py
@@ -0,0 +1,5 @@
+def area(len,br):
+    print("area : ",(len*br))
+    
+def per(len,br):
+    print("area : ",2*(len+br))
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import arithmetic
 from arithmetic import substract
+import geometry
 
 print("Hello world !")
 
 x = int(input("Enter no 1 : "))
 y = int(input("Enter no 2 : "))
 
+
 arithmetic.add(x,y)
 substract(x,y)
+
+geometry.area(7,6)
+geometry.per(7,6)
